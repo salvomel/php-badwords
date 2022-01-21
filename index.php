ddefcc703cda1cb79bb375d626c47567c7c3222e
salvomel <salv.mel@gmail.com>
2022-01-21 14:42:50 +0100

    fatto

--- index.php
+++ index.php
@@ -7,7 +7,25 @@
     <title>Bad words</title>
 </head>
 <body>
+    <h1>PHP Bad Words</h1>
+
+    <?php
+        $frase = 'È meglio tenere la bocca chiusa e lasciare che le persone pensino che sei uno sciocco piuttosto che aprirla e togliere ogni dubbio.';
+        $parola_censored = $_GET['parola'];
+        $frase_censored = str_replace($parola_censored, '***', $frase);
+    ?>
     
+    <h2>Stampo la frase</h2>
+    <p><?php echo $frase; ?></p>
+
+    <h3>Lunghezza della frase:</h3>
+    <p><?php echo strlen($frase); ?></p>
+    
+    <h2>Frase censurata</h2>
+    <p><?php echo $frase_censored; ?></p>
+
+    <h3>Lunghezza della frase censurata:</h3>
+    <p><?php echo strlen($frase_censored); ?></p>
 
 </body>
 </html>
